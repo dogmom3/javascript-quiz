8e934ad56fb3f577fe298fddd37d2773eb2964e0
dogmom3 <blair.seivers@yahoo.com>
2022-01-30 15:52:56 -0500

    js

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,34 +1,13 @@
 var userScore = document.getElementById('user-score');
 var quizTimer = document.getElementById('timer');
 var questionText = document.getElementById('question-text');
-var answer1 = document.getElementById('btn0');
-var answer2 = document.getElementById('btn1');
-var answer3 = document.getElementById('btn2');
+var answer1 = document.getElementById('btn');
+// var answer2 = document.getElementById('btn1');
+// var answer3 = document.getElementById('btn2');
 var quizContainer = document.getElementById('container');
 
 let currentQuestion = 0;
-var score = 0;
-
-
-function showQuestion(question){
-    questionText.innerText = question.question
-    question.answers.forEach(answer =>{
-        var button = document.createElement('button')
-        button.innerText = answer.innerText
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appenChild(button)
-    })
-}
-
-
-
-function nextQuestion(){
-
-}
+var score = 0
 
 //questions and answers
 let questions = [
@@ -74,43 +53,83 @@ let questions = [
     }
 ];
 
+function showQuestion(question) {
+    questionText.innerText = question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement('button')
+        button.innerText = answer.innerText
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appenChild(button)
+    })
+}
+
+function checkAnswer(answer) {
+    if (
+        questions[currentQuestion].answers === questions[currentQuestion].option[answer]
+    ) {
+        correctAns++;
+        answerCheck.textContent = "correct";
+    } else {
+        totalTime -= 10;
+        timeLeft.textContent = totalTime;
+        answerCheck.textContent =
+            "Wrong! The correct answer was: " + questions[currentQuestion].answers;
+    }
+
+    currentQuestion++;
+    if (currentQuestion < questions.length) {
+        nextQuestion();
+    } else {
+        gameOver();
+    }
+}
+
+function gameOver(){
+    
+}
+
+
 //event listeners for button clicks
-btn1.addEventListener('click', btn1);
-btn2.addEventListener('click', btn2);
-btn3.addEventListener('click', btn3);
+btn.addEventListener('click', btn);
+// btn2.addEventListener('click', btn2);
+// btn3.addEventListener('click', btn3);
 
 questionText.innerHTML = questions[currentQuestion].question;
-btn1.setAttribute("data-correct", questionText[currentQuestion].answers[0].answer)
-btn2.setAttribute("data-correct", questionText[currentQuestion].answers[1].answer)
-btn3.setAttribute("data-correct", questionText[currentQuestion].answers[2].answer)
+btn.setAttribute("data-correct", questionText[currentQuestion].answers[0].answer)
+// btn2.setAttribute("data-correct", questionText[currentQuestion].answers[1].answer)
+// btn3.setAttribute("data-correct", questionText[currentQuestion].answers[2].answer)
 
 // event listeners for the answer button clicks
-btn1.addEventListener('click', function (event) {
-console.log(event.target.dataset.correct)
-if (event.target.dataset.correct === 'true') {
-    userScore = score + 1
-    currentQuestion++
-    console.log(currentQuestion, quizLength)
-    //if current question is < questions length then begin quiz
-    if (currentQuestion < quizLength) {
-        beginQuiz();
+btn.addEventListener('click', function (event) {
+    console.log(event.target.dataset.correct)
+    if (event.target.dataset.correct === 'true') {
+        userScore = score + 1
+        currentQuestion++
+        console.log(currentQuestion, quizLength)
+        //if current question is < questions length then begin quiz
+        if (currentQuestion < quizLength) {
+            beginQuiz();
+        }
+        else {
+            //write gameover function that asks for conditions
+            alert('Game over! Your score is ' + score)
+        }
     }
     else {
-        //write gameover function that asks for conditions
-        alert('Game over! Your score is ' + score)
+        currentQuestion++;
+        if (currentQuestion < quizLength) {
+            beginQuiz();
+        }
+        else {
+            //write gameover function that asks for conditions
+            alert('Game over ' + score)
+        }
     }
-}
-else {
-    currentQuestion++;
-    if (currentQuestion < quizLength) {
-        beginQuiz();
-    }
-    else {
-        //write gameover function that asks for conditions
-        alert('Game over ' + score)
-    }
-}
-    });
+});
 
 
 //function to begin the quiz
@@ -118,7 +137,7 @@ function beginQuiz() {
     // currentQuestion = 0;
     // questionText.innerHTML = questions[currentQuestion].question;
     // btnOne.innerHTML = questions[currentQuestion].answers[0].option;
-    btn1.onclick = () => {
+    btn.onclick = () => {
         let ano = 0;
         if (questions[currentQuestion].answers[ano].answer) {
             if (score < 3) {
@@ -129,34 +148,34 @@ function beginQuiz() {
         if (currentQuestion < 2) {
             next();
         }
-    }
-    btn2.innerHTML = questions[currentQuestion].answers[0].option;
-    btn2.onclick = () => {
-        let ano = 1;
-        if (questions[currentQuestion].answers[ano].answer) {
-            if (score < 3) {
-                score++;
-            }
-        }
-        userScore.innerHTML = score;
-        if (currentQuestion < 2) {
-            next();
-        }
-    }
-    btn3.innerHTML = questions[currentQuestion].answers[0].option;
-    btn3.onclick = () => {
-        let ano = 2;
-        if (questions[currentQuestion].answers[ano].answer) {
-            if (score < 3) {
-                score++;
-            }
-        }
-        userScore.innerHTML = score;
-        if (currentQuestion < 2) {
-            next();
-        }
-    }
-}
+    };
+//     btn2.innerHTML = questions[currentQuestion].answers[0].option;
+//     btn2.onclick = () => {
+//         let ano = 1;
+//         if (questions[currentQuestion].answers[ano].answer) {
+//             if (score < 3) {
+//                 score++;
+//             }
+//         }
+//         userScore.innerHTML = score;
+//         if (currentQuestion < 2) {
+//             next();
+//         }
+//     }
+//     btn3.innerHTML = questions[currentQuestion].answers[0].option;
+//     btn3.onclick = () => {
+//         let ano = 2;
+//         if (questions[currentQuestion].answers[ano].answer) {
+//             if (score < 3) {
+//                 score++;
+//             }
+//         }
+//         userScore.innerHTML = score;
+//         if (currentQuestion < 2) {
+//             next();
+//         }
+//     }
+// }
 
 // function next(){
 //     currentQuestion++;
@@ -165,8 +184,8 @@ function beginQuiz() {
 //     }
 // }
 
-//function call for beginQuiz
-beginQuiz();
+// //function call for beginQuiz
+// beginQuiz();
 
 
 // var quizLength = quiz.length
@@ -230,17 +249,4 @@ beginQuiz();
 //     btn1.setAttribute("data-correct", question[currentQuestion].answers[0].answer)
 //     btn2.setAttribute("data-correct", question[currentQuestion].answers[1].answer)
 //     btn3.setAttribute("data-correct", question[currentQuestion].answers[2].answer)
-// }
-
-//timer function
-
-
-//show score function
-// function showScore() {
-//     const answerContainers = quizContainer.querySelectorAll('.answers');
-//     let numCorrect = 0;
-// }
-
-// function showScore() {
-//     var intitials = getDocumentById('score');
-// }
+//}
