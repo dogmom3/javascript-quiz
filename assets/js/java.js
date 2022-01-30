5fa50ad35bb539fa758ae0ea767ec114fa1645ba
dogmom3 <blair.seivers@yahoo.com>
2022-01-30 11:57:07 -0500

    i dont know

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,56 +1,224 @@
-var userScore = document.getElementById('score');
-var timeCount = document.getElementById('timer');
-var questionText = document.getElementById('question');
-var btn1 = document.getElementById('btn1');
-var btn2 = document.getElementById('btn2');
-var btn3 = document.getElementById('btn3');
-var btn4 = document.getElementById('btn4');
-var checkAnswer = document.getElementById('right-wrong');
+var userScore = document.getElementById('user-score');
+var quizTimer = document.getElementById('timer');
+var questionText = document.getElementById('question-text');
+var btnOne = document.getElementById('btn1');
+var btnTwo = document.getElementById('btn2');
+var btnThree = document.getElementById('btn3');
+var quizContainer = document.getElementById('container');
 
 let currentQuestion = 0;
 var score = 0;
 
+
+//questions and answers
 let questions = [
     {
-        question: "Is Javascript difficult to learn?",
+        question: "Who invented Javascript?",
         answers: [
-            {option:"Yes",answer:false},
-            {option:"No",answer:false},
-            {option:"Hell yeah!",answer:false},
-            {option:"The worst!",answer:true}
+            { option: "Waldo Puppito", answer: false },
+            { option: "Clark Griswold", answer: false },
+            { option: "Brendan Eich", answer: true }
         ]
     },
     {
-        question: "What day is it?",
+        question: "What year was Javascript invented?",
         answers: [
-            {option: "Today",answer:false},
-            {option: "Tomorrow",answer:false},
-            {option: "Yesterday",answer:false},
-            {option: "Who knows?!",answer:true}
+            { option: "1990", answer: false },
+            { option: "1995", answer: true },
+            { option: "2002", answer: false }
         ]
     },
     {
-        question: "When was my last shower?",
+        question: "Which is used to check if two variables are equal?",
         answers: [
-            {option: "Yesterday",answer:false},
-            {option: "2 days ago",answer:false},
-            {option: "4 days ago",answer:false},
-            {option: "I have no idea",answer:true}
+            { option: "===", answer: true },
+            { option: "==", answer: false },
+            { option: "=", answer: false }
+        ]
+    },
+    {
+        question: "Which of the following is something that Javascript cannot do?",
+        answers: [
+            { option: "Make interactive websites", answer: false },
+            { option: "Design phone apps", answer: false },
+            { option: "Brew coffee", answer: true }
+        ]
+    },
+    {
+        question: "What does MVP stand for in reference to Javascript?",
+        answers: [
+            { option: "Maximum View Port", answer: false },
+            { option: "Minimal Viable Product", answer: true },
+            { option: "Most Visited Page", answer: false }
         ]
     }
-]
-//not sure if this is correct
-btn1.addEventListener('click',function(event){
-console.log(event.target.dataset.correct)
+];
 
-});
-btn2.addEventListener('click',false);
-btn3.addEventListener('click',false);
-btn4.addEventListener('click',true);
+//event listeners for button clicks
+btnOne.addEventListener('click', btn1);
+btnTwo.addEventListener('click', btn2);
+btnThree.addEventListener('click', btn3);
 
+questionText.innerHTML = questions[currentQuestion].question;
+   btnOne.setAttribute("data-correct", questionText[currentQuestion].answers[0].answer)
+   btnTwo.setAttribute("data-correct", questionText[currentQuestion].answers[1].answer)
+   btnThree.setAttribute("data-correct", questionText[currentQuestion].answers[2].answer)
+
+
+//function to begin the quiz
 function beginQuiz(){
-    currentQuestion = 0;
-    questionText.innerHTML = questions[currentQuestion].question;
-    btn1.setAttribute("data-correct", questions[currentQuestion].answers[0].answer)
-    
+    // currentQuestion = 0;
+    // questionText.innerHTML = questions[currentQuestion].question;
+    // btnOne.innerHTML = questions[currentQuestion].answers[0].option;
+    btnOne.onclick = () => {
+        let ano=0;
+        if(questions[currentQuestion].answers[ano].answer){
+            if(score < 3){
+                score++;
+            }
+        }
+        userScore.innerHTML = score;
+        if(currentQuestion<2){
+            next();
+        }
+    }
+    btnTwo.innerHTML = questions[currentQuestion].answers[0].option;
+    btnTwo.onclick = () => {
+        let ano=1;
+        if(questions[currentQuestion].answers[ano].answer){
+            if(score<3){
+                score++;
+            }
+        }
+        userScore.innerHTML = score;
+        if(currentQuestion<2){
+            next();
+        }
+    }
+    btnThree.innerHTML = questions[currentQuestion].answers[0].option;
+    btnThree.onclick = () => {
+        let ano =2;
+        if(questions[currentQuestion].answers[ano].answer){
+            if(score<3){
+                score++;
+            }
+        }
+        userScore.innerHTML = score;
+        if(currentQuestion<2){
+            next();
+        }
+    }
 }
+
+// function next(){
+//     currentQuestion++;
+//     if(currentQuestion>=2){
+
+//     }
+// }
+
+//function call for beginQuiz
+beginQuiz();
+
+
+// var quizLength = quiz.length
+
+// // event listeners for the answer button clicks
+// btn1.addEventListener('click', function (event) {
+//     console.log(event.target.dataset.correct)
+//     if (event.target.dataset.correct === 'true') {
+//         score = score + 1
+//         currentQuestion++
+//         console.log(currentQuestion, quizLength)
+//         //if current question is < questions length then begin quiz
+//         if (currentQuestion < quizLength) {
+//             beginQuiz();
+//         }
+//         else {
+//             //write gameover function that asks for conditions
+//             alert('Game over! Your score is ' + score)
+//         }
+//     }
+//     else {
+//             currentQuestion++;
+//             if (currentQuestion < quizLength) {
+//                 beginQuiz();
+//             }
+//             else {
+//                 //write gameover function that asks for conditions
+//                 alert('Game over ' + score)
+//             }
+//         }
+//     });
+// btn2.addEventListener('click', function (event) {
+//     console.log(event.target.dataset.correct)
+//     if (event.target.dataset.correct === 'true') {
+//         score = score + 1
+//         currentQuestion++
+//         console.log(currentQuestion, quizLength)
+//         //if current question is < questions length then begin quiz
+//         if (currentQuestion < quizLength) {
+//             beginQuiz();
+//         }
+//         else {
+//             //write gameover function that asks for conditions
+//             alert('Game over ' + score)
+//         }
+//     }
+//     else {
+//             currentQuestion++;
+//             if (currentQuestion < quizLength) {
+//                 beginQuiz();
+//             }
+//             else {
+//                 //write gameover function that asks for conditions
+//                 alert('Game over ' + score)
+//             }
+//         }
+// });
+// btn3.addEventListener('click', function (event) {
+//     console.log(event.target.dataset.correct)
+//     if (event.target.dataset.correct === 'true') {
+//         score = score + 1
+//         currentQuestion++
+//         console.log(currentQuestion, quizLength)
+//         //if current question is < questions length then begin quiz
+//         if (currentQuestion < quizLength) {
+//             beginQuiz();
+//         }
+//         else {
+//             //write gameover function that asks for conditions
+//             alert('Game over')
+//         }
+//     }
+//     else {
+//             currentQuestion++;
+//             if (currentQuestion < quizLength) {
+//                 beginQuiz();
+//             }
+//             else {
+//                 //write gameover function that asks for conditions
+//                 alert('Game over')
+//             }
+//         }
+//     });
+
+
+//     questionText.innerHTML = questions[currentQuestion].question;
+//     btn1.setAttribute("data-correct", question[currentQuestion].answers[0].answer)
+//     btn2.setAttribute("data-correct", question[currentQuestion].answers[1].answer)
+//     btn3.setAttribute("data-correct", question[currentQuestion].answers[2].answer)
+// }
+
+//timer function
+
+
+//show score function
+// function showScore() {
+//     const answerContainers = quizContainer.querySelectorAll('.answers');
+//     let numCorrect = 0;
+// }
+
+// function showScore() {
+//     var intitials = getDocumentById('score');
+// }
