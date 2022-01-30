bd5a149bff21167117c54ffe32850b99e8b5017e
dogmom3 <blair.seivers@yahoo.com>
2022-01-30 14:33:56 -0500

    update variables

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,14 +1,34 @@
 var userScore = document.getElementById('user-score');
 var quizTimer = document.getElementById('timer');
 var questionText = document.getElementById('question-text');
-var btnOne = document.getElementById('btn1');
-var btnTwo = document.getElementById('btn2');
-var btnThree = document.getElementById('btn3');
+var answer1 = document.getElementById('btn0');
+var answer2 = document.getElementById('btn1');
+var answer3 = document.getElementById('btn2');
 var quizContainer = document.getElementById('container');
 
 let currentQuestion = 0;
 var score = 0;
 
+
+function showQuestion(question){
+    questionText.innerText = question.question
+    question.answers.forEach(answer =>{
+        var button = document.createElement('button')
+        button.innerText = answer.innerText
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appenChild(button)
+    })
+}
+
+
+
+function nextQuestion(){
+
+}
 
 //questions and answers
 let questions = [
@@ -55,56 +75,84 @@ let questions = [
 ];
 
 //event listeners for button clicks
-btnOne.addEventListener('click', btn1);
-btnTwo.addEventListener('click', btn2);
-btnThree.addEventListener('click', btn3);
+btn1.addEventListener('click', btn1);
+btn2.addEventListener('click', btn2);
+btn3.addEventListener('click', btn3);
 
 questionText.innerHTML = questions[currentQuestion].question;
-   btnOne.setAttribute("data-correct", questionText[currentQuestion].answers[0].answer)
-   btnTwo.setAttribute("data-correct", questionText[currentQuestion].answers[1].answer)
-   btnThree.setAttribute("data-correct", questionText[currentQuestion].answers[2].answer)
+btn1.setAttribute("data-correct", questionText[currentQuestion].answers[0].answer)
+btn2.setAttribute("data-correct", questionText[currentQuestion].answers[1].answer)
+btn3.setAttribute("data-correct", questionText[currentQuestion].answers[2].answer)
+
+// event listeners for the answer button clicks
+btn1.addEventListener('click', function (event) {
+console.log(event.target.dataset.correct)
+if (event.target.dataset.correct === 'true') {
+    userScore = score + 1
+    currentQuestion++
+    console.log(currentQuestion, quizLength)
+    //if current question is < questions length then begin quiz
+    if (currentQuestion < quizLength) {
+        beginQuiz();
+    }
+    else {
+        //write gameover function that asks for conditions
+        alert('Game over! Your score is ' + score)
+    }
+}
+else {
+    currentQuestion++;
+    if (currentQuestion < quizLength) {
+        beginQuiz();
+    }
+    else {
+        //write gameover function that asks for conditions
+        alert('Game over ' + score)
+    }
+}
+    });
 
 
 //function to begin the quiz
-function beginQuiz(){
+function beginQuiz() {
     // currentQuestion = 0;
     // questionText.innerHTML = questions[currentQuestion].question;
     // btnOne.innerHTML = questions[currentQuestion].answers[0].option;
-    btnOne.onclick = () => {
-        let ano=0;
-        if(questions[currentQuestion].answers[ano].answer){
-            if(score < 3){
+    btn1.onclick = () => {
+        let ano = 0;
+        if (questions[currentQuestion].answers[ano].answer) {
+            if (score < 3) {
                 score++;
             }
         }
         userScore.innerHTML = score;
-        if(currentQuestion<2){
+        if (currentQuestion < 2) {
             next();
         }
     }
-    btnTwo.innerHTML = questions[currentQuestion].answers[0].option;
-    btnTwo.onclick = () => {
-        let ano=1;
-        if(questions[currentQuestion].answers[ano].answer){
-            if(score<3){
+    btn2.innerHTML = questions[currentQuestion].answers[0].option;
+    btn2.onclick = () => {
+        let ano = 1;
+        if (questions[currentQuestion].answers[ano].answer) {
+            if (score < 3) {
                 score++;
             }
         }
         userScore.innerHTML = score;
-        if(currentQuestion<2){
+        if (currentQuestion < 2) {
             next();
         }
     }
-    btnThree.innerHTML = questions[currentQuestion].answers[0].option;
-    btnThree.onclick = () => {
-        let ano =2;
-        if(questions[currentQuestion].answers[ano].answer){
-            if(score<3){
+    btn3.innerHTML = questions[currentQuestion].answers[0].option;
+    btn3.onclick = () => {
+        let ano = 2;
+        if (questions[currentQuestion].answers[ano].answer) {
+            if (score < 3) {
                 score++;
             }
         }
         userScore.innerHTML = score;
-        if(currentQuestion<2){
+        if (currentQuestion < 2) {
             next();
         }
     }
@@ -123,33 +171,7 @@ beginQuiz();
 
 // var quizLength = quiz.length
 
-// // event listeners for the answer button clicks
-// btn1.addEventListener('click', function (event) {
-//     console.log(event.target.dataset.correct)
-//     if (event.target.dataset.correct === 'true') {
-//         score = score + 1
-//         currentQuestion++
-//         console.log(currentQuestion, quizLength)
-//         //if current question is < questions length then begin quiz
-//         if (currentQuestion < quizLength) {
-//             beginQuiz();
-//         }
-//         else {
-//             //write gameover function that asks for conditions
-//             alert('Game over! Your score is ' + score)
-//         }
-//     }
-//     else {
-//             currentQuestion++;
-//             if (currentQuestion < quizLength) {
-//                 beginQuiz();
-//             }
-//             else {
-//                 //write gameover function that asks for conditions
-//                 alert('Game over ' + score)
-//             }
-//         }
-//     });
+// /
 // btn2.addEventListener('click', function (event) {
 //     console.log(event.target.dataset.correct)
 //     if (event.target.dataset.correct === 'true') {
